108ea2b378025a5439b8781f41660c4468bce98f
bingxuan liu <bingxuan.liu@cern.ch>
2023-08-09 18:10:12 +0200

    Update delphiAnalysis.py

--- delphiAnalysis.py
+++ delphiAnalysis.py
@@ -1,4 +1,4 @@
-mport ROOT
+import ROOT
 import os,sys,re
 import numpy as np
 import math
@@ -9,33 +9,48 @@ ROOT.gInterpreter.Declare('#include "external/ExRootAnalysis/ExRootTreeReader.h"
 outf = ROOT.TFile("output.root","RECREATE")
 mass = ROOT.TH1D("mass","mass",30,0,1500)
 minDPhi = ROOT.TH1D("minDPhi","minDPhi",32,0,3.2)
-dPhi = ROOT.TH1D("dPhi","dPhi",32,0,3.2)
+leadjet_pt = ROOT.TH1D("pt_lead","pt_lead",20,0,1000)
+subleadjet_pt = ROOT.TH1D("pt_sublead","pt_sublead",20,0,1000)
+dPhi_sublead = ROOT.TH1D("dPhi_sublead","dphi_sublead",32,0,3.2)
+dPhi_lead = ROOT.TH1D("dPhi_lead","dPhi_lead",32,0,3.2)
+dPhi_2D = ROOT.TH2D("dPhi_2D","dPhi_2D", 32,0,3.2, 32,0,3.2)
+dPhi_y = ROOT.TH1D("dPhi_y","dPhi_y",32,0,3.2)
 
 chain = ROOT.TChain("Delphes")
-chain.Add("Events/run_01/Z500_rinv0p5.root")
+chain.Add("Events/run_01/Z500_GammaISR500_rinv0p5.root")
 
 # Create object of class ExRootTreeReader
 treeReader = ROOT.ExRootTreeReader(chain)
 numberOfEntries = treeReader.GetEntries()
+branchWeight = treeReader.UseBranch("Weight")
 branchJet = treeReader.UseBranch("Jet")
 branchMuon = treeReader.UseBranch("Muon")
 branchPhoton = treeReader.UseBranch("Photon")
 branchMet = treeReader.UseBranch("MissingET")
 
 for entry in range(0, numberOfEntries):
-  # Load selected branches with data from specified event
+
   treeReader.ReadEntry(entry)
+
+  weight = branchWeight.At(0).Weight
 
   lead_j = ROOT.TLorentzVector()
   sublead_j = ROOT.TLorentzVector()
 
-  # If event contains at least 1 jet
   for j in range(0,  branchJet.GetEntries()):
     jet = branchJet.At(j)
+    if jet.PT < 25:
+      continue
+    if abs(jet.Eta) > 2.5:
+      continue
     if jet.PT > lead_j.Pt():
       lead_j.SetPtEtaPhiM(jet.PT, jet.Eta, jet.Phi, jet.Mass)
     if jet.PT < lead_j.Pt() and jet.PT > sublead_j.Pt():
       sublead_j.SetPtEtaPhiM(jet.PT, jet.Eta, jet.Phi, jet.Mass)
+
+  leadjet_pt.Fill(lead_j.Pt(), weight)
+  subleadjet_pt.Fill(sublead_j.Pt(), weight)
+
 
   if branchJet.GetEntries() < 2:
     continue
@@ -51,13 +66,22 @@ for entry in range(0, numberOfEntries):
       photon.SetPtEtaPhiM(pho.PT, pho.Eta, pho.Phi, 0)
 
 
-  minDPhi.Fill(min([lead_j.DeltaPhi(met), sublead_j.DeltaPhi(met)]))
-  dPhi.Fill(met.DeltaPhi(photon))
+  minDPhi.Fill(min([lead_j.DeltaPhi(met), sublead_j.DeltaPhi(met)]), weight)
+  dPhi_lead.Fill(lead_j.DeltaPhi(met), weight)
+  dPhi_sublead.Fill(sublead_j.DeltaPhi(met), weight)
+  dPhi_2D.Fill(lead_j.DeltaPhi(met), sublead_j.DeltaPhi(met), weight)
+  dPhi_y.Fill(met.DeltaPhi(photon), weight)
 
-  mass.Fill((lead_j + sublead_j ).M())
+  mass.Fill((lead_j + sublead_j ).M(), weight)
 
 outf.cd()
 mass.Write()
 minDPhi.Write()
-dPhi.Write()
+dPhi_lead.Write()
+dPhi_sublead.Write()
+dPhi_2D.Write()
+dPhi_y.Write()
+leadjet_pt.Write()
+subleadjet_pt.Write()
 outf.Close()
+~                  
